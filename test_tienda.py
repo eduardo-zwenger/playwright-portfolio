4f9107d17dbae7b98c3ba301b02bbf731302810f
Eduardo <eduardozwenger@hotmail.com>
2026-06-20 19:42:06 +0200

    fix: cambiar url para evitar el bloqueo de bots en CI

--- test_tienda.py
+++ test_tienda.py
@@ -1,11 +1,11 @@
 from playwright.sync_api import Page, expect
 
-def test_cargar_pagina(page):
-    # 1. Entramos a la página de prácticas
+def test_cargar_pagina(page: Page):
+    # 1. Entramos a una página de prácticas libre de bloqueos
     print("\nAbriendo la página de pruebas...")
-    page.goto("https://automationexercise.com")
+    page.goto("https://the-internet.herokuapp.com")
     
     # 2. Validamos que el título de la pestaña sea el correcto
     print("Validando el título...")
-    expect(page).to_have_title("Automation Exercise")
+    expect(page).to_have_title("The Internet")
     print("¡Prueba completada con éxito! 🎉")
